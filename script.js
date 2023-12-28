eb8801b49351c978556f291e499526761ae6582c
Jaka Černetič <jaka.cernetic@students.vegova.si>
2023-12-28 21:38:46 +0100

    dost ok

--- script.js
+++ script.js
@@ -1,241 +1,236 @@
 // set up the game board
 const board = [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0]
+	[0, 0, 0, 0],
+	[0, 0, 0, 0],
+	[0, 0, 0, 0],
+	[0, 0, 0, 0],
 ];
 
 // add a random tile to the board
 function addRandomTile() {
-    const emptyTiles = [];
-    for (let row = 0; row < board.length; row++) {
-        for (let col = 0; col < board[row].length; col++) {
-            if (board[row][col] === 0) {
-                emptyTiles.push({ row, col });
-            }
-        }
-    }
-    const { row, col } = emptyTiles[Math.floor(Math.random() * emptyTiles.length)];
-    board[row][col] = Math.random() < 0.9 ? 2 : 4;
+	const emptyTiles = [];
+	for (let row = 0; row < board.length; row++) {
+		for (let col = 0; col < board[row].length; col++) {
+			if (board[row][col] === 0) {
+				emptyTiles.push({ row, col });
+			}
+		}
+	}
+	const { row, col } =
+		emptyTiles[Math.floor(Math.random() * emptyTiles.length)];
+	board[row][col] = Math.random() < 0.9 ? 2 : 4;
 }
 
 // render the game board
 function render() {
-    const boardElement = document.getElementById('board');
-    boardElement.innerHTML = '';
-    for (let row = 0; row < board.length; row++) {
-        for (let col = 0; col < board[row].length; col++) {
-            const tile = document.createElement('div');
-            tile.className = 'tile';
-            tile.classList.add(`tile-${board[row][col]}`);
-            tile.innerText = board[row][col] === 0 ? '' : board[row][col];
-            tile.style.top = `${row * 100}px`;
-            tile.style.left = `${col * 100}px`;
-            boardElement.appendChild(tile);
-        }
-    }
+	const boardElement = document.getElementById("board");
+	boardElement.innerHTML = "";
+	for (let row = 0; row < board.length; row++) {
+		for (let col = 0; col < board[row].length; col++) {
+			const tile = document.createElement("div");
+			tile.className = "tile";
+			tile.classList.add(`tile-${board[row][col]}`);
+			tile.innerText = board[row][col] === 0 ? "" : board[row][col];
+			tile.style.top = `${row * 100}px`;
+			tile.style.left = `${col * 100}px`;
+			boardElement.appendChild(tile);
+		}
+	}
 }
-
-let points = 0;
-const score = document.getElementById('score');
 
 // move tiles on the board
 function moveTiles(direction) {
-    let moved = false;
-    for (let row = 0; row < board.length; row++) {
-        for (let col = 0; col < board[row].length; col++) {
-            if (board[row][col] !== 0) {
-                switch (direction) {
-                    case 'up':
-                        if (row > 0 && board[row - 1][col] === 0) {
-                            board[row - 1][col] = board[row][col];
-                            board[row][col] = 0;
-                            moved = true;
-                            points = +[row - 1][col];
-                        }
-                        break;
-                    case 'down':
-                        if (row < board.length - 1 && board[row + 1][col] === 0) {
-                            board[row + 1][col] = board[row][col];
-                            board[row][col] = 0;
-                            moved = true;
-                            points = +[row + 1][col];
-                        }
-                        break;
-                    case 'left':
-                        if (col > 0 && board[row][col - 1] === 0) {
-                            board[row][col - 1] = board[row][col];
-                            board[row][col] = 0;
-                            moved = true;
-                            points = +[row][col - 1];
-                        }
-                        break;
-                    case 'right':
-                        if (col < board[row].length - 1 && board[row][col + 1] === 0) {
-                            board[row][col + 1] = board[row][col];
-                            board[row][col] = 0;
-                            moved = true;
-                            points = +[row][col + 1];
-                        }
-                        break;
-                }
-            }
-        }
-    }
-    return moved;
+	let moved = false;
+	for (let row = 0; row < board.length; row++) {
+		for (let col = 0; col < board[row].length; col++) {
+			if (board[row][col] !== 0) {
+				switch (direction) {
+					case "up":
+						if (row > 0 && board[row - 1][col] === 0) {
+							board[row - 1][col] = board[row][col];
+							board[row][col] = 0;
+							moved = true;
+						}
+						break;
+					case "down":
+						if (
+							row < board.length - 1 &&
+							board[row + 1][col] === 0
+						) {
+							board[row + 1][col] = board[row][col];
+							board[row][col] = 0;
+							moved = true;
+						}
+						break;
+					case "left":
+						if (col > 0 && board[row][col - 1] === 0) {
+							board[row][col - 1] = board[row][col];
+							board[row][col] = 0;
+							moved = true;
+						}
+						break;
+					case "right":
+						if (
+							col < board[row].length - 1 &&
+							board[row][col + 1] === 0
+						) {
+							board[row][col + 1] = board[row][col];
+							board[row][col] = 0;
+							moved = true;
+						}
+						break;
+				}
+			}
+		}
+	}
+	return moved;
 }
-
-score.innerText = points;
 
 // merge tiles on the board
 function mergeTiles(direction) {
-    let merged = false;
-    for (let row = 0; row < board.length; row++) {
-        for (let col = 0; col < board[row].length; col++) {
-            if (board[row][col] !== 0) {
-                switch (direction) {
-                    case 'up':
-                        if (row > 0 && board[row - 1][col] === board[row][col]) {
-                            board[row - 1][col] *= 2;
-                            board[row][col] = 0;
-                            merged = true;
-                        }
-                        break;
-                    case 'down':
-                        if (row < board.length - 1 && board[row + 1][col] === board[row][col]) {
-                            board[row + 1][col] *= 2;
-                            board[row][col] = 0;
-                            merged = true;
-                        }
-                        break;
-                    case 'left':
-                        if (col > 0 && board[row][col - 1] === board[row][col]) {
-                            board[row][col - 1] *= 2;
-                            board[row][col] = 0;
-                            merged = true;
-                        }
-                        break;
-                    case 'right':
-                        if (col < board[row].length - 1 && board[row][col + 1] === board[row][col]) {
-                            board[row][col + 1] *= 2;
-                            board[row][col] = 0;
-                            merged = true;
-                        }
-                        break;
-                }
-            }
-        }
-    }
-    return merged;
+	let merged = false;
+	for (let row = 0; row < board.length; row++) {
+		for (let col = 0; col < board[row].length; col++) {
+			if (board[row][col] !== 0) {
+				switch (direction) {
+					case "up":
+						if (
+							row > 0 &&
+							board[row - 1][col] === board[row][col]
+						) {
+							board[row - 1][col] *= 2;
+							board[row][col] = 0;
+							merged = true;
+						}
+						break;
+					case "down":
+						if (
+							row < board.length - 1 &&
+							board[row + 1][col] === board[row][col]
+						) {
+							board[row + 1][col] *= 2;
+							board[row][col] = 0;
+							merged = true;
+						}
+						break;
+					case "left":
+						if (
+							col > 0 &&
+							board[row][col - 1] === board[row][col]
+						) {
+							board[row][col - 1] *= 2;
+							board[row][col] = 0;
+							merged = true;
+						}
+						break;
+					case "right":
+						if (
+							col < board[row].length - 1 &&
+							board[row][col + 1] === board[row][col]
+						) {
+							board[row][col + 1] *= 2;
+							board[row][col] = 0;
+							merged = true;
+						}
+						break;
+				}
+			}
+		}
+	}
+	return merged;
 }
 
 // handle key presses
 function handleKeyPress(event) {
-    let moved = false;
-    let merged = false;
-    switch (event.key) {
-        case 'ArrowUp':
-            moved = moveTiles('up');
-            merged = mergeTiles('up');
-            moveTiles('up');
-            break;
-        case 'ArrowDown':
-            moved = moveTiles('down');
-            merged = mergeTiles('down');
-            moveTiles('down');
-            break;
-        case 'ArrowLeft':
-            moved = moveTiles('left');
-            merged = mergeTiles('left');
-            moveTiles('left');
-            break;
-        case 'ArrowRight':
-            moved = moveTiles('right');
-            merged = mergeTiles('right');
-            moveTiles('right');
-            break;
-    }
-    if (moved || merged) {
-        addRandomTile();
-        render();
-    }
+	let moved = false;
+	let merged = false;
+	switch (event.key) {
+		case "ArrowUp":
+			moved = moveTiles("up");
+			merged = mergeTiles("up");
+			moveTiles("up");
+			break;
+		case "ArrowDown":
+			moved = moveTiles("down");
+			merged = mergeTiles("down");
+			moveTiles("down");
+			break;
+		case "ArrowLeft":
+			moved = moveTiles("left");
+			merged = mergeTiles("left");
+			moveTiles("left");
+			break;
+		case "ArrowRight":
+			moved = moveTiles("right");
+			merged = mergeTiles("right");
+			moveTiles("right");
+			break;
+	}
+	if (moved || merged) {
+		addRandomTile();
+		render();
+	}
 }
 
 // start the game
 addRandomTile();
 addRandomTile();
 render();
-window.addEventListener('keydown', handleKeyPress);
+window.addEventListener("keydown", handleKeyPress);
 
 //win
 function win() {
-    let win = false;
-    for (let row = 0; row < board.length; row++) {
-        for (let col = 0; col < board[row].length - 1; col++) {
-            if (board[row][col] === 2048) {
-                win = true;
-            }
-        }
-    }
-    if (win) {
-        alert('You Win!');
-    }
+	let win = false;
+	for (let row = 0; row < board.length; row++) {
+		for (let col = 0; col < board[row].length - 1; col++) {
+			if (board[row][col] === 2048) {
+				win = true;
+			}
+		}
+	}
+	if (win) {
+		alert("You Win!");
+	}
 }
 
 //listen for buttons pressed
-const up = document.getElementById('up');
-const down = document.getElementById('down');
-const left = document.getElementById('left');
-const right = document.getElementById('right');
+const up = document.getElementById("up");
+const down = document.getElementById("down");
+const left = document.getElementById("left");
+const right = document.getElementById("right");
 
-up.addEventListener('click', () => {
-    moveTiles('up');
-    mergeTiles('up');
-    moveTiles('up');
-    addRandomTile();
-    render();
-    win();
+up.addEventListener("click", () => {
+	moveTiles("up");
+	mergeTiles("up");
+	moveTiles("up");
+	addRandomTile();
+	render();
+	win();
 });
 
-down.addEventListener('click', () => {
-    moveTiles('down');
-    mergeTiles('down');
-    moveTiles('down');
-    addRandomTile();
-    render();
-    win();
+down.addEventListener("click", () => {
+	moveTiles("down");
+	mergeTiles("down");
+	moveTiles("down");
+	addRandomTile();
+	render();
+	win();
 });
 
-left.addEventListener('click', () => {
-    moveTiles('left');
-    mergeTiles('left');
-    moveTiles('left');
-    addRandomTile();
-    render();
-    win();
+left.addEventListener("click", () => {
+	moveTiles("left");
+	mergeTiles("left");
+	moveTiles("left");
+	addRandomTile();
+	render();
+	win();
 });
 
-right.addEventListener('click', () => {
-    moveTiles('right');
-    mergeTiles('right');
-    moveTiles('right');
-    addRandomTile();
-    render();
-    win();
+right.addEventListener("click", () => {
+	moveTiles("right");
+	mergeTiles("right");
+	moveTiles("right");
+	addRandomTile();
+	render();
+	win();
 });
-
-//lose
-function lose() {
-    let lose = false;
-    for (let row = 0; row < board.length; row++) {
-        for (let col = 0; col < board.length - 1; col++) {
-            if (board[row][col] === 0) {
-                lose = true;
-            }
-        }
-    }
-    if (!lose) {
-        alert('You Lose!');
-    }
-}
